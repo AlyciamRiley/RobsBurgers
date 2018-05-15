1a761192ac7fa4f949b34741f7a8cb0bdc7eab52
alyciamriley <alyciarileym@gmail.com>
2018-05-14 19:01:59 -0500

    file structure and schema set up.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('bbq', true);

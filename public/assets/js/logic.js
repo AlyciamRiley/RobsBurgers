c4609852bae8d8f4ad07734cfe20a460335e1ca3
alyciamriley <alyciarileym@gmail.com>
2018-05-17 13:20:34 -0500

    will not change devour state.

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,8 +1,35 @@
 // Create new burger
 // ===============================================
 $(function() {
+
+    //Eat burger
+    $(".change-devoured").on("click", function(event) {
+      event.preventDefault();
+      console.log("onclick is working!");
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newDevoured");
+
+      var newDevouredState = {
+          devoured: newDevoured
+      };
+
+    //   Send the PUT Request
+      $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newDevouredState
+      }).then(
+          function() {
+              console.log("changed devoured to ", newDevoured);
+              location.reload();
+          }
+      );
+  });
+
+
+    //Create new burger
   $("#submit").on("click", function(event) {
     event.preventDefault();
+    
     //Create new burger
     var newBurger = {
       burger_name: $("#ca")
@@ -20,4 +47,14 @@ $(function() {
       location.reload();
     });
   });
+
+
+
+
+
+
+
+
+
+
 });

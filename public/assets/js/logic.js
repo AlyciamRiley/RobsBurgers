0b4c6b6266b769f71699fb0ed7c41f9acf8ee8a8
alyciamriley <alyciarileym@gmail.com>
2018-07-19 11:38:10 -0500

    correct state/ids pulling, but if else is defaulting to else function

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,36 +1,10 @@
 // Create new burger
 // ===============================================
 $(function() {
-
-    //Eat burger
-    $("#change-devoured").on("click", function(event) {
-      event.preventDefault();
-      var id = $(this).data("id");
-      console.log("ID", id)
-      var newDevoured = $(this).data("newdevoured");
-
-      var newDevouredState = {
-          devoured: newDevoured
-      };
-      console.log("new devoured", newDevoured)
-
-    //   Send the PUT Request
-      $.ajax("/api/burgers/" + id, {
-          type: "PUT",
-          data: newDevouredState
-      }).then(
-          function() {
-              console.log("changed devoured to ", newDevoured);
-              location.reload();
-          }
-      );
-  });
-
-
-    //Create new burger
+  //Create new burger
   $("#submit").on("click", function(event) {
     event.preventDefault();
-    
+
     //Create new burger
     var newBurger = {
       burger_name: $("#ca")
@@ -50,15 +24,56 @@ $(function() {
   });
 
 
-  //Throw Away Burger
-  
+  $(".change-devoured").on("click", function(event) {
+    var newDevoured = $(this).attr("data-newdevoured");
 
+    console.log("onclick is working")
+    console.log(newDevoured)
+    event.preventDefault();
+    if (newDevoured === true) {
+      eatBurger();
+      
+    }
+     else {
+      deleteBurger();
+    }
+  });
 
+  function eatBurger() {
+    //Eat burger
+    var id = $(".change-devoured").attr("data-id");
+    console.log("ID", id);
 
+    //check if its true or false
 
+    //
 
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+    console.log("new devoured", newDevoured);
 
+    //   Send the PUT Request
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(function() {
+      console.log("changed devoured to ", newDevoured);
+      location.reload();
+    });
+  }
 
-
-
+  // Throw Away Burger
+  function deleteBurger() {
+    var id = $(".change-devoured").attr("data-id");
+    // Send the DELETE request-- put in the "else" statement
+    
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(function() {
+      console.log("deleted burger!", id);
+      //reload page to get the updated list
+      location.reload();
+    });
+  }
 });

6012da7a9479d2ba274f7385c87d031adf776ea4
alyciamriley <alyciarileym@gmail.com>
2018-07-21 13:44:08 -0500

    functionality is good to go.  Have to figure out what that tiny button is.

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -23,47 +23,40 @@ $(function() {
     });
   });
 
-
-  var newDevoured = $(".change-devoured").attr("data-newdevoured");
-  var id = $(".change-devoured").attr("data-id");
-
   $(".change-devoured").on("click", function(event) {
-
-    console.log(newDevoured);
     event.preventDefault();
-    if (newDevoured == true) {
-      eatBurger();
-    } else {
-      deleteBurger();
-    }
-  });
+    var id = $(this).data("id");
+    console.log("ID", id)
+    var newDevoured = $(this).data("newdevoured");
 
-  function eatBurger() {
     var newDevouredState = {
-      devoured: newDevoured
+        devoured: newDevoured
     };
-    console.log("new devoured", newDevoured);
+    console.log("new devoured", newDevoured)
 
-    //   Send the PUT Request
+  //   Send the PUT Request
     $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newDevouredState
-    }).then(function() {
-      console.log("changed devoured to ", newDevoured);
-      // location.reload();
-    });
-  }
+        type: "PUT",
+        data: newDevouredState
+    }).then(
+        function() {
+            console.log("changed devoured to ", newDevoured);
+            // location.reload();
+        }
+    );
+});
 
-  // Throw Away Burger
-  function deleteBurger() {
 
-    // Send the delete request
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(function() {
-      console.log("deleted burger!", id);
-      //reload page to get the updated list
-      // location.reload();
-    });
-  }
+$(".throw-away").on("click", function(event) {
+  event.preventDefault();
+  var id = $(".throw-away").attr("data-id");
+  $.ajax("/api/burgers/" + id, {
+    type: "DELETE"
+  }).then(function() {
+    console.log("deleted burger!", id);
+    //reload page to get the updated list
+    // 
+})
+});
+
 });
